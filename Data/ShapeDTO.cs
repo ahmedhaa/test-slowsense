23c0911aa6d07fba58791505ba34c6657aeaf409
ahmed hamdaoui <127329175+ahmedhaa@users.noreply.github.com>
2024-12-26 18:57:33 +0100

    Add files via upload

---
+++ Data/ShapeDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace shapesapp.Data
+{
+    public class ShapeDTO
+    {
+        public Guid Id { get; set; }
+        public int FormsTypeId { get; set; }
+        public string FormsTypeName { get; set; } // Le nom du type de forme
+        public decimal PositionX { get; set; }
+        public decimal PositionY { get; set; }
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public string? Text { get; set; }
+        public int Order { get; set; }
+    }
+}
